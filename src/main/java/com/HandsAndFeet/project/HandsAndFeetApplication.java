208238340034af2d0d216bc1fa176009d394d2ba
Ivan Polipas <ivanpolipas@Ivans-MacBook-Air.local>
2024-01-21 18:25:03 -0500

    initialize spring boot project

---
+++ src/main/java/com/HandsAndFeet/project/HandsAndFeetApplication.java
@@ -0,0 +1,13 @@
+package com.HandsAndFeet.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsAndFeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsAndFeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HandsAndFeet/project/HandsAndFeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HandsAndFeet.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsAndFeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
