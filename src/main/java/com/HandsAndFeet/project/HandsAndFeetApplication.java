052e85e97b100c671d23140eb66c26a7ea181958
Ivan Polipas <ivanpolipas@Ivans-MacBook-Air.local>
2024-07-05 20:20:06 -0400

    test commit

--- src/main/java/com/HandsAndFeet/project/HandsAndFeetApplication.java
+++ src/main/java/com/HandsAndFeet/project/HandsAndFeetApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HandsAndFeetApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(HandsAndFeetApplication.class, args);
 	}
 
